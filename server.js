d030722f9919960db0e34c4e3d5871f0d392eb1c
adambouteldja <ademtk2007@gmail.com>
2026-06-06 02:12:29 +0100

    Use environment variable for server port

--- server.js
+++ server.js
@@ -115,6 +115,8 @@ ${answer}`;
     }
 });
 
-app.listen(3000, () => {
-    console.log('server running');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('server running on ' + PORT);
 });
